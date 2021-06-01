836c8e7ecf75a2fd237eea59b8e05ecb195a69ec
Saumya-Bhatt <saumya.bhatt106@gmail.com>
2021-06-01 11:47:14 +0530

    Removed file storage to admin panel

--- api/routes/operations.js
+++ api/routes/operations.js
@@ -1,6 +1,4 @@
 const express  = require('express');
-const multer = require('multer')
-const path = require('path')
 const { v4 : uuidv4 } = require('uuid')
 const database = require('../../firebase');
 const router   = express.Router();
@@ -8,23 +6,6 @@ require('dotenv').config();
 
 
 
-
-
-// file upload code
-var storage = multer.diskStorage({ //multers disk storage settings
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads/'))
-    },
-    filename: function (req, file, cb) {
-        // console.log(file);
-        var datetimestamp = Date.now();
-        cb(null, file.originalname)
-    }
-});
-
-var uploadSingle = multer({ //multer settings
-    storage: storage
-}).single('poster');
 
 
 
@@ -43,50 +24,51 @@ function writeReviewData(table, data) {
     return id
 }
 
+
+
 router.post('/upload',  (req, res, next) => {
-    uploadSingle(req, res, function(err) {
-        if(err) {
-            res.json({
-                error_code : 1,
-                error_desc : err
-            });
-            return;
-        } 
-        poster_path = (req.file.path).split('/').slice(-1).pop()
-        let data = {
-            name      : req.body.name,
-            director  : req.body.director,
-            genre     : [req.body.genre1, req.body.genre2],
-            instagram : req.body.instagram.toLowerCase() == 'true',
-            review    : req.body.review,
-            year      : parseInt(req.body.year),
-            actor     : req.body.actor,
-            netflix   : req.body.netflix.toLowerCase() == 'true',
-            amazon    : req.body.amazon.toLowerCase() == 'true',
-            acting    : parseFloat(req.body.acting),
-            story     : parseFloat(req.body.story),
-            execution : parseFloat(req.body.execution),
-            profundity: parseFloat(req.body.profundity),
-            overall   : parseFloat(req.body.overall),
-            trailer   : req.body.trailer,
-            poster_name    : poster_path
-        };
-        let id = writeReviewData(req.body.table, data);
-        return res
-            .status(200)
-            .json({
-                message : 'Successfully added to Movie-Reviews Database',
-                request : {
-                    type : 'POST',
-                    url : process.env.SERVER + '/upload'
-                },
-                response : {
-                    uuid : id,
-                    table : req.body.table,
-                    data_uploaded : data
-                }
-            })
-    })
+    let data = {
+        name       : req.body.name,
+        director   : req.body.director,
+        genre      : req.body.genre,
+        instagram  : req.body.instagram,
+        poster_name: req.body.poster_name,
+        poster_link: req.body.poster_link
+    };
+    let table = req.body.table;
+    if (table == 'movie-reviews') {
+        data.review     = req.body.review;
+        data.year       = req.body.year;
+        data.actor      = req.body.actor;
+        data.netflix    = req.body.netflix;
+        data.amazon     = req.body.amazon;
+        data.acting     = req.body.acting;
+        data.story      = req.body.story;
+        data.execution  = req.body.execution;
+        data.profundity = req.body.profundity;
+        data.overall    = req.body.overall;
+        data.trailer    = req.body.trailer;
+    }
+    else if (table == 'short-film-reviews') {
+        data.link        = req.body.link;
+        data.description = req.body.description;
+        data.duration    = req.body.duration;
+    }
+    let id = writeReviewData(req.body.table, data)
+    return res
+        .status(200)
+        .json({
+            message: 'Successfully added to Movie-Reviews Database',
+            request: {
+                type: 'POST',
+                url : process.env.SERVER + '/upload'
+            },
+            response : {
+                uuid         : id,
+                table        : table,
+                data_uploaded: data
+            }
+        })
 })
 
 //=======================================================================================
