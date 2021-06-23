6ba50e0f1246f0e978fc111add8a782993700aa3
Saumya-Bhatt <saumya.bhatt106@gmail.com>
2021-06-23 09:12:49 +0530

    Updated general route

--- models/reviews.js
+++ models/reviews.js
@@ -75,22 +75,51 @@ async function getReviewByID(req, res) {
 //=====================================================================================
 //=====================================================================================
 
-async function getData(table, query) {
+async function getData(table, query, allInfo=false) {
   const data = {
     size    : 0,
     response: [],
   };
   let   counter   = 0;
-  const reviewRef = db.collection(String(table));
+  const reviewRef = db.collection(table);
   const snap      = await reviewRef
       .where(query, "==", true)
       .get();
   snap.forEach((doc) => {
-    data.response.push(doc.id);
+    if(allInfo) {
+      data.response.push({
+        id  : doc.id,
+        data: doc.data()
+      });
+    }
     counter++;
   });
   data.size = counter;
   return data;
+}
+
+
+async function getDataGenre(table, query, allInfo=false) {
+  const data = {
+    size    : 0,
+    response: []
+  };
+  let   counter   = 0;
+  const reviewRef = db.collection(table);
+  const snap      = await reviewRef
+      .where("genre","array-contains",query)
+      .get();
+    snap.forEach((doc) => {
+      if(allInfo) {
+        data.response.push({
+          id  : doc.id,
+          data: doc.data()
+        });
+      }
+      counter++;
+    });
+    data.size = counter;
+    return data
 }
 
 
@@ -103,6 +132,9 @@ async function getGeneralInfo(req, res) {
     request: {
       type: "GET",
       url : process.env.SERVER + "/reviews" + "/general",
+      body: {
+        table: req.body.table
+      }
     },
     response: {
       Instagram : await getData(req.body.table, "instagram"),
@@ -110,6 +142,26 @@ async function getGeneralInfo(req, res) {
       Prime     : await getData(req.body.table, "prime"),
       Must_Watch: await getData(req.body.table, "must_watch"),
       Foreign   : await getData(req.body.table, "foreign"),
+      Genre     : {
+        Lighthearted : await getDataGenre(req.body.table, "Lighthearted"),
+        Comedy       : await getDataGenre(req.body.table, "Comedy"),
+        Romance      : await getDataGenre(req.body.table, "Romance"),
+        Horror       : await getDataGenre(req.body.table, "Horror"),
+        Thriller     : await getDataGenre(req.body.table, "Thriller"),
+        Animated     : await getDataGenre(req.body.table, "Animated"),
+        Dark         : await getDataGenre(req.body.table, "Dark"),
+        Meta         : await getDataGenre(req.body.table, "Meta"),
+        War          : await getDataGenre(req.body.table, "War"),
+        Crime        : await getDataGenre(req.body.table, "Crime"),
+        Inspirational: await getDataGenre(req.body.table, "Inspirational"),
+        Sci_Fi       : await getDataGenre(req.body.table, "Sci_Fi"),
+        True_Story   : await getDataGenre(req.body.table, "True_Story"),
+        Drama        : await getDataGenre(req.body.table, "Drama"),
+        Fantasy      : await getDataGenre(req.body.table, "Fantasy"),
+        Action       : await getDataGenre(req.body.table, "Action"),
+        Indie        : await getDataGenre(req.body.table, "Indie"),
+        Mystery      : await getDataGenre(req.body.table, "Mystery"),
+      }
     },
   });
 }
