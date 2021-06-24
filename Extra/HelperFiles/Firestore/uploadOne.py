a05bd80817b89cb27621f99f034ee47d5f1357b1
Saumya-Bhatt <saumya.bhatt106@gmail.com>
2021-06-25 01:03:50 +0530

    Added helper files README

--- Extra/HelperFiles/Firestore/uploadOne.py
+++ Extra/HelperFiles/Firestore/uploadOne.py
@@ -25,28 +25,28 @@ else:
 
 
 item =  {
-   "Name": "The Lighthouse",
-   "Review": "Set in the early 20th century, this is a movie of two men who are stuck at a lighthouse island and their gradual descent into madness. Robert Eggers very clearly flexes his impeccable sense of direction here. It creates an eerie sense of unsettling while keeping you drawn to the fantastic setting. What boosts this prospect is that the movie is shot in square frame and is in BnW. As all greate psychological thrillers, this too has a quite bit of symbolism which like I suggest in my previous reviews encourage you to read more about the movie after it ends to actually feel the message that the story writer wanted to send across. But what truely makes this a masterpiece is the oscar worthy acting by both Williem Defoe and Robert Pattinson (who may have have proved his mettle as an actor with this movie). They could easily have won an oscar (Defoe should've gotten for supporting actor imo) had it not been for a certain Joker. Overall this is powerfull watch which would dazzle you with it's acting, setting and symbolism rather than the story itself. Reserve it for those cold winter evenings and watch as it acts out the insanities and depravities of man when forced into isolation (hey!).",
-   "Instagram": 1,
-   "Netflix": 0,
-   "Prime": 0,
-   "Year": 2019,
-   "Director": "Robert Eggers",
-   "Lead1": "Robert Pattinson",
-   "Lead2": "Williem Defoe",
-   "Lead3": "Logan Hawkes",
-   "Acting": "4.4",
-   "Story": 3.2,
-   "Execution": 4,
+   "Name"      : "The Lighthouse",
+   "Review"    : "Set in the early 20th century, this is a movie of two men who are stuck at a lighthouse island and their gradual descent into madness. Robert Eggers very clearly flexes his impeccable sense of direction here. It creates an eerie sense of unsettling while keeping you drawn to the fantastic setting. What boosts this prospect is that the movie is shot in square frame and is in BnW. As all greate psychological thrillers, this too has a quite bit of symbolism which like I suggest in my previous reviews encourage you to read more about the movie after it ends to actually feel the message that the story writer wanted to send across. But what truely makes this a masterpiece is the oscar worthy acting by both Williem Defoe and Robert Pattinson (who may have have proved his mettle as an actor with this movie). They could easily have won an oscar (Defoe should've gotten for supporting actor imo) had it not been for a certain Joker. Overall this is powerfull watch which would dazzle you with it's acting, setting and symbolism rather than the story itself. Reserve it for those cold winter evenings and watch as it acts out the insanities and depravities of man when forced into isolation (hey!).",
+   "Instagram" : 1,
+   "Netflix"   : 0,
+   "Prime"     : 0,
+   "Year"      : 2019,
+   "Director"  : "Robert Eggers",
+   "Lead1"     : "Robert Pattinson",
+   "Lead2"     : "Williem Defoe",
+   "Lead3"     : "Logan Hawkes",
+   "Acting"    : "4.4",
+   "Story"     : 3.2,
+   "Execution" : 4,
    "Profundity": 2,
-   "Overall": 3.4,
-   "Poster": "lighthouse.jpg",
-   "Foreign": 0,
+   "Overall"   : 3.4,
+   "Poster"    : "lighthouse.jpg",
+   "Foreign"   : 0,
    "Must_Watch": 0,
-   "Genre1": "Dark",
-   "Genre2": "Thriller",
-   "Genre3": "Meta",
-   "Trailer": "https://www.youtube.com/watch?v=Hyag7lR8CPA"
+   "Genre1"    : "Dark",
+   "Genre2"    : "Thriller",
+   "Genre3"    : "Meta",
+   "Trailer"   : "https://www.youtube.com/watch?v=Hyag7lR8CPA"
  }
 
 fileName = item['Poster']
