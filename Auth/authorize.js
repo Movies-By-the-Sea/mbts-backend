83f8846a84c12a7ba8a60d7fd3b6a3c870cd6b03
Saumya-Bhatt <saumya.bhatt106@gmail.com>
2021-07-02 16:08:02 +0530

    Removed unwanted files

--- Auth/authorize.js
+++ Auth/authorize.js
@@ -31,8 +31,19 @@ async function sharedAuthorize(uid, author_uid, level) {
     return false;
 }
 
+function isAuthenticated(req, res, next) {
+    if((req.body.email !== undefined) && (req.body.password != undefined)) {
+        return next();
+    }
+    if(req.body.uid === undefined) {
+        return res.status(401).json({message : "Unauthorized"});
+    }
+    return next();
+}
+
 
 module.exports = {
     isAuthorized   : isAuthorized,
+    isAuthenticated: isAuthenticated,
     sharedAuthorize: sharedAuthorize
 };
--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,22 @@
-const express         = require('express');
-const asyncHandler    = require('express-async-handler');
-const isAuthenticated = require("../Auth/authenticate");
-const user            = require("../models/users");
-const router          = express.Router();
+const express      = require('express');
+const asyncHandler = require('express-async-handler');
+const auth         = require("../Auth/authorize");
+const user         = require("../models/users");
+const router       = express.Router();
 
 
 
 // THESE ROUTES ARE ACCESSIBLE BY ANY USER HAVING VALID UID TOKENS
 router.get("/",
-    asyncHandler((req, res, next) => isAuthenticated(req, res, next)),
+    asyncHandler((req, res, next) => auth.isAuthenticated(req, res, next)),
     asyncHandler((req, res)       => user.getInfo(req, res))
 );
 router.put("/update",
-    asyncHandler((req, res, next) => isAuthenticated(req, res, next)),
+    asyncHandler((req, res, next) => auth.isAuthenticated(req, res, next)),
     asyncHandler((req, res)       => user.updateInfo(req, res))
 )
 router.get("/posts",
-    asyncHandler((req, res, next) => isAuthenticated(req, res, next)),
+    asyncHandler((req, res, next) => auth.isAuthenticated(req, res, next)),
     asyncHandler((req, res)       => user.getUserPosts(req, res))
 );
 
