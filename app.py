9e2538d15df674a4be672c0e842ca1df32a97647
Saumya Bhatt <saumya.bhatt106@gmail.com>
2022-02-09 10:52:12 +0530

    Added CORS bypass

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 from controller.movies import movie_url
@@ -7,6 +8,7 @@ load_dotenv('../config/.env')
 
 app = Flask(__name__)
 app.register_blueprint(movie_url)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
